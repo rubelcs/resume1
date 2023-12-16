5650164804a4f88ab44b5ea27a516142171e8790
ansaryshohan <ansarye2083@gmaail.com>
2023-12-16 15:40:06 +0600

    navbar issue fixed

--- js/index.js
+++ js/index.js
@@ -3,29 +3,34 @@ const closeMenu = document.getElementById("close");
 const menu = document.getElementById("menu");
 const everyLi = document.querySelectorAll("#menu li");
 
-function menuStyle(hamburgerStyle, closeMenuStyle, menuStyle,anchorStyle) {
+function menuStyle(hamburgerStyle, closeMenuStyle, menuStyle, anchorStyle) {
   hamburger.style.display = hamburgerStyle;
   closeMenu.style.display = closeMenuStyle;
   menu.style.width = menuStyle;
-  everyLi.forEach(li => {
-    const aTag= li.children[0];
-    aTag.style.display=anchorStyle;
+  everyLi.forEach((li) => {
+    const aTag = li.children[0];
+    aTag.style.display = anchorStyle;
   });
 }
 function iconToggle() {
   hamburger.addEventListener("click", () => {
-    menuStyle("none", "block", "300px" ,"block");
+    menuStyle("none", "block", "300px", "block");
   });
   closeMenu.addEventListener("click", () => {
-    menuStyle("block", "none", "0px","none");
+    menuStyle("block", "none", "0px", "none");
   });
   menu.addEventListener("click", () => {
-    menuStyle("block", "none", "0px","none");
+    menuStyle("block", "none", "0px", "none");
   });
   everyLi.forEach((li) =>
     li.addEventListener("click", () => {
-      menuStyle("block", "none", "0px","none");
+      menuStyle("block", "none", "0px", "none");
     })
   );
+  if (document.defaultView.window.innerWidth > 992) {
+    menuStyle("none", "none", 'full', "block");
+  }
 }
 iconToggle();
+console.log(document.defaultView)
+

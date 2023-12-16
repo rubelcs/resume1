724f61bbe7b8be49a71d6028cfb384d14515a75a
ansaryshohan <ansarye2083@gmaail.com>
2023-12-16 15:15:52 +0600

    navbar issue fixed

--- js/index.js
+++ js/index.js
@@ -3,21 +3,28 @@ const closeMenu = document.getElementById("close");
 const menu = document.getElementById("menu");
 const everyLi = document.querySelectorAll("#menu li");
 
-function menuStyle(hamburgerStyle, closeMenuStyle, menuStyle) {
+function menuStyle(hamburgerStyle, closeMenuStyle, menuStyle,anchorStyle) {
   hamburger.style.display = hamburgerStyle;
   closeMenu.style.display = closeMenuStyle;
   menu.style.width = menuStyle;
+  everyLi.forEach(li => {
+    const aTag= li.children[0];
+    aTag.style.display=anchorStyle;
+  });
 }
 function iconToggle() {
   hamburger.addEventListener("click", () => {
-    menuStyle("none", "block", "300px");
+    menuStyle("none", "block", "300px" ,"block");
   });
   closeMenu.addEventListener("click", () => {
-    menuStyle("block", "none", "0px");
+    menuStyle("block", "none", "0px","none");
+  });
+  menu.addEventListener("click", () => {
+    menuStyle("block", "none", "0px","none");
   });
   everyLi.forEach((li) =>
     li.addEventListener("click", () => {
-      menuStyle("block", "none", "0px");
+      menuStyle("block", "none", "0px","none");
     })
   );
 }

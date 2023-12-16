5308c2dfba40b1512196a94b317075416a5cd924
ansaryshohan <ansarye2083@gmaail.com>
2023-12-17 04:35:13 +0600

    testimonial section ends

--- js/index.js
+++ js/index.js
@@ -32,5 +32,39 @@ function iconToggle() {
   }
 }
 iconToggle();
-console.log(document.defaultView)
 
+// =========testimonial slider==
+
+const btn= document.getElementsByClassName('btn');
+const slide= document.getElementById('slide');
+
+const testimonialSlider=()=>{
+  
+btn[0].onclick=function(){
+  slide.style.transform="translateX(0px)";
+  for(i=0;i<3;i++){
+    btn[i].classList.remove('active-btn');
+  }
+  // console.log(this)
+  this.classList.add("active-btn")
+}
+btn[1].onclick=function(){
+  slide.style.transform="translateX(-800px)";
+  for(i=0;i<3;i++){
+    btn[i].classList.remove('active-btn');
+  }
+  // console.log(this)
+  this.classList.add("active-btn")
+}
+btn[2].onclick=function(){
+  slide.style.transform="translateX(-1600px)"
+
+  for(i=0;i<3;i++){
+    btn[i].classList.remove('active-btn');
+  }
+  // console.log(this)
+  this.classList.add("active-btn")
+}
+
+};
+testimonialSlider();
